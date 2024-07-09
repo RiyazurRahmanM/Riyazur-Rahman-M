4576ec5bffe7536728be939a1f006155b00c84e2
RiyazurRahmanM <devriyaz29@gmail.com>
2024-07-09 12:26:32 +0530

    Implement Dice class with roll method

---
+++ Desktop/MagicalArena/src/com/magicalarena/Dice.java
@@ -0,0 +1,11 @@
+package com.magicalarena;
+
+import java.util.Random;
+
+public class Dice {
+    private static final Random random = new Random();
+
+    public static int roll() {
+        return random.nextInt(6) + 1;
+    }
+}
