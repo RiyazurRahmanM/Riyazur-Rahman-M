a5a4b36777724f57b9a8df6c9a2eecdf0451678c
RiyazurRahmanM <devriyaz29@gmail.com>
2024-07-09 12:25:26 +0530

    Implement Player class with basic attributes and methods

--- Desktop/MagicalArena/src/com/magicalarena/Player.java
+++ Desktop/MagicalArena/src/com/magicalarena/Player.java
@@ -1,5 +1,39 @@
 package com.magicalarena;
 
 public class Player {
+    private String playerName;
+    private int health;
+    private int strength;
+    private int attack;
 
+    public Player(String playerName, int health, int strength, int attack) {
+        this.playerName = playerName;
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public boolean isAlive() {
+        return health > 0;
+    }
 }
